34a05a11dec54375ae146a5d0797d191c8c5298e
WladRamos <wladimirramos88@gmail.com>
2024-08-12 21:25:21 -0300

    first commit

---
+++ AI_Assistant/Assistant_app/management/commands/add_data_chroma.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+import chromadb
+import pandas as pd
+from pathlib import Path
+
+
+class Command(BaseCommand):
+    help = 'Adiciona dados de trabalhos freelancer ao ChromaDB'
+
+    def handle(self, *args, **options):
+
+        base_dir = Path(__file__).resolve().parent.parent.parent
+
+        caminho_csv = base_dir / 'dados/dados_tratados.csv'
+
+        caminho_persistent_client = base_dir.parent
+
+        df = pd.read_csv(caminho_csv, dtype=str)
+
+        client = chromadb.PersistentClient(path=str(caminho_persistent_client))
+        collection = client.create_collection("Base_de_Trabalhos")
+
+        documents = df['Job Title'].tolist()
+        metadatas = df.to_dict(orient='records')
+        ids = df.index.astype(str).tolist()
+
+        collection.add(
+            documents=documents,
+            metadatas=metadatas,
+            ids=ids
+        )
+
+        print("Dados inseridos com sucesso no Chroma DB!")
