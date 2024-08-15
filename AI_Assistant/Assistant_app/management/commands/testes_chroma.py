c6e4a74492ca7e80bf759e47a16b4eba0dee4c49
WladRamos <wladimirramos88@gmail.com>
2024-08-15 17:48:58 -0300

    Refine de Titulo para Query

--- AI_Assistant/Assistant_app/management/commands/testes_chroma.py
+++ AI_Assistant/Assistant_app/management/commands/testes_chroma.py
@@ -5,6 +5,24 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 import chromadb
+import re
+
+PROMPT_QUERY_ANALISE = """"
+Você será responsável por receber detalhes sobre um trabalho de freelance na área de programação e responder com um possível título para esse trabalho.
+Esse título será Utilizado para fazer uma pesquisa para encontrar trabalhos semelhantes.
+
+    Instruções:
+
+    1. Caso na descrição do trabalho informado ja tenha um título para o trabalho, você pode utilizá-lo como resposta.
+
+    2. Sua resposta deve conter o título para o trabalho na sua língua original e também em inglês, separados por um '-'. Caso a língua original ja seja inglês, deverá ser retornado apenas um título.
+
+    3. Sua resposta deve estar formatada da seguinte forma:
+
+    ```resposta
+    Título em Inglês - Título na Língua Original
+    ```
+"""
 
 class Command(BaseCommand):
     help = 'Testa integração do chroma com o gpt'
@@ -21,41 +39,72 @@ class Command(BaseCommand):
         client = chromadb.PersistentClient(path=str(caminho_persistent_client))
         collection = client.get_collection("Base_de_Trabalhos")
 
-        # Configurando embeddings
-        embeddings = OpenAIEmbeddings()
-
         # Configurando o LLM (modelo de linguagem)
         llm = ChatOpenAI(model_name="gpt-4o-mini")
 
-        def chatbot(usuario_input):
-            print("Verificando documentos no vector store...")
-
-            # Executa a busca diretamente na coleção
-            results = collection.query(
-                query_texts=[usuario_input],
-                n_results=5  # Especifique quantos resultados você quer retornar
-            )
+        def coleta_titulo(titulo):
+            padrao = r'```resposta(.*?)```'
+            resultado = re.search(padrao, titulo, re.DOTALL)
             
-            docs = results['documents'][0]  # Acessa a lista de documentos retornada
-            print(f"Documentos encontrados: {len(docs)}")
-            for doc in docs:
-                print(doc)
+            if resultado:
+                return resultado.group(1).strip()
+            else:
+                return titulo
 
-            if not docs:
-                return "Nenhum documento relevante encontrado."
+
+        def chatbot(usuario_input):
+
+            prompt_titulo = PROMPT_QUERY_ANALISE + usuario_input
+
+            titulos = coleta_titulo(llm(prompt_titulo).content).split("-")
+
+            print("Titulo Recebido pela LLM")
+            print(titulos)
+
+            for titulo in titulos:
+                results = collection.query(
+                    query_texts=[titulo],
+                    n_results=5
+                )
+                docs = results # Acessa a lista de documentos retornada
+                print(docs)
+
+                if not docs:
+                    return "Nenhum documento relevante encontrado."
 
             # Prepara o contexto para o prompt
-            context = "\n\nContexto:\n" + "\n".join(docs)
-            prompt = usuario_input + context
+            #context = "\n\nContexto:\n" + "\n".join(docs)
+            #prompt = usuario_input + context
 
             # Gera a resposta usando o contexto recuperado
-            resposta = llm(prompt)
-            return resposta
+            #resposta = llm(prompt)
+            #return resposta
 
         # Exemplo de uso
-        usuario_input = """Descreva as informações do job que mais se encaixa com as especificações do usuário:
+        usuario_input = """
+            SMS with link shortening + Linux Admin
+            Posted 5 minutes ago
+            Worldwide
+            Im looking to create a simple system for now. The simple system will be sending and receiving SMS messages and using our own(that you need to create) link shortening service.
 
-        Especificação do usuário: Gostaria de um trabalho que pague por hora para fazer uma landing page.
+            I noticed that backend devs are usually not so good wit front-end and thats fine ill get a designer and front-end developer separately.
+
+            For backend i prefer if you know basic Linux administration, DB administration,.
+
+            Bonus if you have experience with GSM Modems or SMS API.
+
+
+
+            More than 30 hrs/week
+            Hourly
+            Intermediate
+            Experience Level
+            $20.00-$45.00 Hourly
+            Remote Job
+            Ongoing project
+            Project Type
+            Contract-to-hire
+            This job has the potential to turn into a full time role
         """
         resposta = chatbot(usuario_input)
         print(resposta)
