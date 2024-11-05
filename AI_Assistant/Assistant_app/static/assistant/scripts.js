21c4f5426b86d5c411bf309a58d0503e21e8aa9e
WladRamos <wladimirramos88@gmail.com>
2024-11-05 14:27:03 -0300

    bug do csrf resolvido

--- AI_Assistant/Assistant_app/static/assistant/scripts.js
+++ AI_Assistant/Assistant_app/static/assistant/scripts.js
@@ -13,20 +13,6 @@ document.addEventListener('DOMContentLoaded', function(){
         }, 0);
     }
 
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
 
     // Função para enviar mensagem com fetch
     function sendMessage() {
